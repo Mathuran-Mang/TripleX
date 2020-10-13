1645214d684526059b24a966b62ecefff02d0278
Mathuran-Mang <45081636+Mathuran-Mang@users.noreply.github.com>
2020-10-13 19:51:46 -0400

    Initial Commit

---
+++ TripleX.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You're a wizard trapped inside a dungeon, trying to escape numerous magical monsters and beasts...\n";
+    std::cout << "You come across a giant stone gate which has writing on it...\n";
+    std::cout << "The writing says to put the runes you see at the bottom of the gate in the correct order to pass...";
+    
+    int a = 4;
+    int b = 7;
+    int c = 8;
+
+    int sum = a + b + c;
+
+    std::cout << std::endl << sum;
+    
+    
+    return 0;
+}
