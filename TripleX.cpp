39af1ed8500b49edbfcd84b2789d26c4c41bed90
Mathuran-Mang <45081636+Mathuran-Mang@users.noreply.github.com>
2020-10-14 20:37:53 -0400

    Added more content to game

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,15 +5,19 @@ int main()
     std::cout << "You're a wizard trapped inside a dungeon, trying to escape numerous magical monsters and beasts...\n";
     std::cout << "You come across a giant stone gate which has writing on it...\n";
     std::cout << "The writing says to put the runes you see at the bottom of the gate in the correct order to pass...";
-    
-    int a = 4;
-    int b = 7;
-    int c = 8;
+     
+    int CodeA = 4;
+    int CodeB = 7;
+    int CodeC = 8;
 
-    int sum = a + b + c;
-
-    std::cout << std::endl << sum;
     
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << std::endl;
+    std::cout << std::endl << "-There are three numbers in the code";
+    std::cout << std::endl << "-The codes add-up to: " << CodeSum;
+    std::cout << std::endl << "-The codes multiply to: " << CodeProduct;
     
     return 0;
 }
